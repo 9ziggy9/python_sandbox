8d2bc18fb8b6b8157eff375d99e967563f17a060
Ziggy <davidarogers@protonmail.com>
2022-04-24 23:52:19 -0700

    lel, algebra ftw

--- stats.py
+++ stats.py
@@ -8,3 +8,9 @@ def sample_variance(*nums):
     avg = average(*nums)
     sV = sum([(num - avg) ** 2 for num in nums]) / (len(nums) - 1)
     return sV
+
+# returns value that must be appended to list to force a given avg
+def force_average_with(target, *nums):
+    avg = average(*nums)
+    # (avg * len(nums) + new_number) / (len(nums) + 1) = target
+    return (len(nums) + 1) * target - avg * len(nums)
