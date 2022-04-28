e0e13ed936d56feecb87596b9687b2a914a20761
Ziggy <davidarogers@protonmail.com>
2022-04-27 20:38:52 -0700

    crazy recursive sample space generator

--- stats.py
+++ stats.py
@@ -1,5 +1,7 @@
+import numpy as np
 import math
 import collections
+from functools import reduce
 
 def mean(*nums):
     try:
@@ -31,7 +33,7 @@ def v_force_average(target, *nums):
     # (avg * len(nums) + new_number) / (len(nums) + 1) = target
     return (len(nums) + 1) * target - avg * len(nums)
 
-def v_median(*nums):
+def median(*nums):
     sorted_nums = sorted(nums)
     center = len(nums) / 2
     if (len(nums) % 2) == 0:
@@ -61,5 +63,59 @@ def three_sigma_range(mu, sigma):
         '99.7%': (mu - 3*sigma, mu + 3*sigma)
     }
 
-def z_score(x, mu, sigma):
+def find_nearest(*nums, val):
+    snums = sorted(nums)
+    nparr = np.asarray(snums)
+    i = (np.abs(nparr - val)).argmin()
+    return nparr[i]
+
+def quartiles(*nums):
+    snums = sorted(nums)
+    q_2 = math.ceil(len(snums) / 2)
+    q_1 = math.ceil(q_2 / 2)
+    q_3 = math.ceil(q_2 + q_1)
+    return { # have to subtract one to get arr index
+            'q1': snums[q_1 - 1],
+            'q2': snums[q_2 - 1],
+            'q3': snums[q_3 - 1]
+    }
+
+def z_score(x, mu, sigma): # aka number of sds away from mean
     return (x - mu) / sigma
+
+def percentile(n,N): #ordinal number n, total number N
+    return 100 * (n/N)
+
+def percentile_from_data(p, *nums):
+    snums = sorted(nums)
+    n = len(snums)
+    l = math.ceil((p / 100) * n)
+    return snums[l-1]
+
+def ordinal_from_percentile(p,N):
+    return math.ceil(p * N / 100)
+
+def cv_from_data(*nums):
+    sigma = sample_standard_deviation(*nums)
+    mu = mean(*nums)
+    return (sigma / mu) * 100
+
+def mean_from_f(*points):
+    fm2_table = []
+    n = sum([p[2] for p in points])
+    for point in points:
+        fm2_table.append(point[2] * ((point[1] + point[0])/2))
+    return sum(fm2_table) / n
+
+def sample_variance_from_f(*points):
+    fm2_table = []
+    mu = mean_from_f(*points)
+    n = sum([p[2] for p in points])
+    for point in points:
+        fm2_table.append(point[2] * ((point[1] + point[0])/2)**2)
+    return (sum(fm2_table) - n*(mu**2))/ (n-1)
+
+# accepts list of exclusive states provided as tuple of chars
+# sample space denoted ss
+def generate_sample_space(*states, ss=[]):
+    if len(states) == 0: return "hello, world!"
